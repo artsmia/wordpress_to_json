da197d66b4190bfa2207875fdb94b9352c4f9dd5
Kjell <kjell@leanside.com>
2013-09-10 13:27:41 -0500

    Retab for my sanity, .editorconfig in vain

--- templates/tdx_africa.php
+++ templates/tdx_africa.php
@@ -8,22 +8,22 @@ $stories = array();
 $notes = array();
 
 $posts = $wpdb->get_results(
-	"SELECT * FROM $wpdb->posts WHERE (post_type = 'object' || post_type = 'story') && post_status = 'publish'"
+  "SELECT * FROM $wpdb->posts WHERE (post_type = 'object' || post_type = 'story') && post_status = 'publish'"
 );
 
 foreach($posts as $post){
-	setup_postdata($post);
-	if($post->post_type == 'object'){
-		// VIEWS
-		$tms_id = get_field('tms_id');
-		$rows = get_field('views');
-		$views = array();
-		for($n=0;$n<count($rows);$n++){
-			//$views[$n]['primary'] = $rows[$n]['primary'];
-			$image = $views[$n]['image'] = $rows[$n]['img_link'];
-			$views[$n]['credit'] = $rows[$n]['credit'];
-			$views[$n]['annotations'] = array();
-			//ANNOTATIONS
+  setup_postdata($post);
+  if($post->post_type == 'object'){
+    // VIEWS
+    $tms_id = get_field('tms_id');
+    $rows = get_field('views');
+    $views = array();
+    for($n=0;$n<count($rows);$n++){
+      //$views[$n]['primary'] = $rows[$n]['primary'];
+      $image = $views[$n]['image'] = $rows[$n]['img_link'];
+      $views[$n]['credit'] = $rows[$n]['credit'];
+      $views[$n]['annotations'] = array();
+      //ANNOTATIONS
       $ch = curl_init();
       curl_setopt($ch, CURLOPT_URL, "https://afrx.firebaseio.com/".$image."/notes2.json");
       curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -33,96 +33,95 @@ foreach($posts as $post){
       curl_close($ch);
 
       $image_notes = array();
-			$annos = $rows[$n]['annotations'];
-			for($i=0;$i<count($annos);$i++){
+      $annos = $rows[$n]['annotations'];
+      for($i=0;$i<count($annos);$i++){
         $views[$n]['annotations'][$i] = $image_notes[$i] = array(
           'description' => $annos[$i]['description'],
           'firebase' => $f_annotations[$i]
         );
-			}
+      }
 
       $notes[$image] = $image_notes;
-      /* echo json_encode($notes); die(); */
-		}
-		// CONNECTIONS
-		$rels = array();
-		$connected = get_posts(
-			array(
-				'connected_type' => 'objects_to_stories',
-				'connected_items' => $post->ID,
-				'nopaging' => true,
-				'suppress_filters' => false
-			)
-		);
-		if($connected){
-			foreach($connected as $connection){
-				$rels[] = $connection->ID;
-			}
-		}
-		// BASIC INFO 
-		$objects[$tms_id] = array(
-			'id' => get_the_ID(),
-			'title' => get_the_title(),
-			'description' => get_field('description'),
-			'tombstone' => get_field('tombstone'),
-			'views' => $views,
-			'relatedStories' => $rels
-		);
-	}
-	if($post->post_type == 'story'){
-		// PAGES
-		$rows = get_field('pages');
-		$pages = array();
-		for($n=0;$n<count($rows);$n++){
-			$pages[$n]['type'] = $rows[$n]['acf_fc_layout'];
-			if($rows[$n]['text']){
-				$pages[$n]['text'] = $rows[$n]['text'];
-			}
-			if($rows[$n]['map']){
-				$pages[$n]['map'] = $rows[$n]['map'];
-			}
-			if($rows[$n]['vid_link']){
-				$pages[$n]['video'] = $rows[$n]['vid_link'];
-			}
-			if($rows[$n]['credit']){
-				$pages[$n]['credit'] = $rows[$n]['credit'];
-			}
-			if($rows[$n]['img_link']){
-				$pages[$n]['image'] = $rows[$n]['img_link'];
-			}
-			if($rows[$n]['img_link_b']){
-				$pages[$n]['imageB'] = $rows[$n]['img_link_b'];
-			}
-		}
-		// CONNECTIONS
-		$rels = array();
-		$connected = get_posts(
-			array(
-				'connected_type' => 'objects_to_stories',
-				'connected_items' => $post->ID,
-				'nopaging' => true,
-				'suppress_filters' => false
-			)
-		);
-		if($connected){
-			foreach($connected as $connection){
-				$rels[] = $connection->ID;
-			}
-		}		
-		// BASIC INFO
-		$stories[] = array(
-			'id' => get_the_ID(),
-			'title' => get_the_title(),
-			'pages' => $pages,
-			'relatedObjects' => $rels
-		);
-	}
-	wp_reset_postdata();
+    }
+    // CONNECTIONS
+    $rels = array();
+    $connected = get_posts(
+      array(
+        'connected_type' => 'objects_to_stories',
+        'connected_items' => $post->ID,
+        'nopaging' => true,
+        'suppress_filters' => false
+      )
+    );
+    if($connected){
+      foreach($connected as $connection){
+        $rels[] = $connection->ID;
+      }
+    }
+    // BASIC INFO
+    $objects[$tms_id] = array(
+      'id' => get_the_ID(),
+      'title' => get_the_title(),
+      'description' => get_field('description'),
+      'tombstone' => get_field('tombstone'),
+      'views' => $views,
+      'relatedStories' => $rels
+    );
+  }
+  if($post->post_type == 'story'){
+    // PAGES
+    $rows = get_field('pages');
+    $pages = array();
+    for($n=0;$n<count($rows);$n++){
+      $pages[$n]['type'] = $rows[$n]['acf_fc_layout'];
+      if($rows[$n]['text']){
+        $pages[$n]['text'] = $rows[$n]['text'];
+      }
+      if($rows[$n]['map']){
+        $pages[$n]['map'] = $rows[$n]['map'];
+      }
+      if($rows[$n]['vid_link']){
+        $pages[$n]['video'] = $rows[$n]['vid_link'];
+      }
+      if($rows[$n]['credit']){
+        $pages[$n]['credit'] = $rows[$n]['credit'];
+      }
+      if($rows[$n]['img_link']){
+        $pages[$n]['image'] = $rows[$n]['img_link'];
+      }
+      if($rows[$n]['img_link_b']){
+        $pages[$n]['imageB'] = $rows[$n]['img_link_b'];
+      }
+    }
+    // CONNECTIONS
+    $rels = array();
+    $connected = get_posts(
+      array(
+        'connected_type' => 'objects_to_stories',
+        'connected_items' => $post->ID,
+        'nopaging' => true,
+        'suppress_filters' => false
+      )
+    );
+    if($connected){
+      foreach($connected as $connection){
+        $rels[] = $connection->ID;
+      }
+    }
+    // BASIC INFO
+    $stories[] = array(
+      'id' => get_the_ID(),
+      'title' => get_the_title(),
+      'pages' => $pages,
+      'relatedObjects' => $rels
+    );
+  }
+  wp_reset_postdata();
 }
 
 $json = array(
-	'objects' => $objects,
-	'stories' => $stories,
+  'objects' => $objects,
+  'stories' => $stories,
   'notes' => $notes
 );
 
